941090d8712cb76beb90cdc7e16bab5652f45dd7
Misaka-Aki <66006338+Misaka-Aki@users.noreply.github.com>
2022-10-19 15:09:20 +0800

    Update report-in-school.py

--- report-in-school.py
+++ report-in-school.py
@@ -2,6 +2,7 @@ import json
 import requests
 import urllib3
 import os
+import datatime
 
 url = "https://eai.buct.edu.cn/ncov/wap/default/save"
 users = [
@@ -81,8 +82,7 @@ def auto_report(user):
         },
     }
 
-    result = s.post(url, data=data, headers=headers,
-                    cookies=user['cookies'], verify=False)
+    result = s.post(url, data=data, headers=headers, cookies=user['cookies'], verify=False)
     print(json.loads(result.text)['m'])
     
     from datetime import datetime
@@ -90,7 +90,7 @@ def auto_report(user):
     print(datetime.now(tz=pytz.timezone('Asia/Shanghai')))
     
     from onepush import notify
-    notify('pushplus', token=os.environ['KEY'], title='OnePush', content=json.loads(result.text)['m'])
+    notify('pushplus', token=os.environ['KEY'], title='OnePush', content=datetime.now(tz=pytz.timezone('Asia/Shanghai')) + json.loads(result.text)['m'])
 
 if __name__ == '__main__':
     for item in users:
